65b5f6ec0acf4dfe606f1a8b81d5d6053ef51916
defrozentruth <metro2033dg@gmail.com>
2025-03-15 01:35:07 +0300

    working profile

--- frontend/app/src/main/java/com/khl_app/ui/screens/client/ProfileScreen.kt
+++ frontend/app/src/main/java/com/khl_app/ui/screens/client/ProfileScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
@@ -28,6 +29,13 @@ import java.time.format.DateTimeFormatter
 
 var ProfilePicture = ""
 var ProfileName = "John Doe"
+var Level = 1
+var Score = 1
+var Forecasts = 1
+var Following = 1
+var Followers = 1
+var WinnerPoints = 1
+var ScorePoints = 1
 
 @Composable
 fun ProfileScreen(
@@ -35,9 +43,9 @@ fun ProfileScreen(
     navHostController: NavHostController,
     isEditable: Boolean = false
 ) {
-    var showImageDialog by remember { mutableStateOf(false) } // Состояние для отображения диалога
+    var showImageDialog by remember { mutableStateOf(false) }
     var showNameDialog by remember { mutableStateOf(false) }
-    var tempUrl by remember { mutableStateOf("") } // Временное хранилище для URL в диалоге
+    var tempUrl by remember { mutableStateOf("") }
     var tempName by remember { mutableStateOf("") }
 
     Column(
@@ -45,11 +53,10 @@ fun ProfileScreen(
             .fillMaxSize()
             .background(color = Color(0xFF2C2F3E))
             .padding(horizontal = 16.dp, vertical = 16.dp),
-        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        // Фиксированная верхняя часть
         ProfileTopBar(onMenuClick = { navHostController.navigate(Screen.MainScreen.route) })
-
         Spacer(modifier = Modifier.height(16.dp))
 
         // Фото профиля
@@ -58,7 +65,7 @@ fun ProfileScreen(
                 .size(240.dp)
                 .clip(CircleShape)
                 .background(Color.DarkGray.copy(alpha = 0.3f))
-                .clickable { showImageDialog = true }, // Открываем диалог при нажатии
+                .clickable { showImageDialog = true },
             contentAlignment = Alignment.Center
         ) {
             if (ProfilePicture != "") {
@@ -70,12 +77,12 @@ fun ProfileScreen(
                             .transformations(CircleCropTransformation())
                             .build()
                     ),
-                    contentDescription = "Profile Picture",
+                    contentDescription = "Фото профиля",
                     modifier = Modifier.size(120.dp)
                 )
             } else {
                 Text(
-                    text = "Add Photo",
+                    text = "Добавить фото",
                     color = Color.White,
                     fontSize = 16.sp
                 )
@@ -86,7 +93,7 @@ fun ProfileScreen(
         if (showImageDialog) {
             AlertDialog(
                 onDismissRequest = { showImageDialog = false },
-                title = { Text("Insert Image URL", color = Color.White) },
+                title = { Text("Вставьте ссылку...", color = Color.White) },
                 text = {
                     BasicTextField(
                         value = tempUrl,
@@ -102,8 +109,8 @@ fun ProfileScreen(
                     TextButton(
                         onClick = {
                             ProfilePicture = tempUrl
-                            tempUrl = "" // Очищаем временное поле
-                            showImageDialog = false // Закрываем диалог
+                            tempUrl = ""
+                            showImageDialog = false
                         }
                     ) {
                         Text("OK", color = Color(0xFF6C5CE7))
@@ -112,15 +119,15 @@ fun ProfileScreen(
                 dismissButton = {
                     TextButton(
                         onClick = {
-                            tempUrl = "" // Очищаем временное поле
-                            showImageDialog = false // Закрываем диалог
+                            tempUrl = ""
+                            showImageDialog = false
                         }
                     ) {
-                        Text("Cancel", color = Color.White)
+                        Text("Отмена", color = Color.White)
                     }
                 },
-                containerColor = Color(0xFF2C2F3E), // Цвет фона диалога
-                textContentColor = Color.White // Цвет текста внутри диалога
+                containerColor = Color(0xFF2C2F3E),
+                textContentColor = Color.White
             )
         }
 
@@ -129,11 +136,11 @@ fun ProfileScreen(
         // Кнопка для смены фото (доступна только в редактируемом режиме)
         if (isEditable) {
             Button(
-                onClick = { showImageDialog = true }, // Открываем диалог вместо прямой логики
+                onClick = { showImageDialog = true },
                 modifier = Modifier.align(Alignment.CenterHorizontally),
                 colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF6C5CE7))
             ) {
-                Text("Change Profile Picture", color = Color.White)
+                Text("Изменить фото профиля", color = Color.White)
             }
         }
 
@@ -155,7 +162,7 @@ fun ProfileScreen(
         if (showNameDialog) {
             AlertDialog(
                 onDismissRequest = { showNameDialog = false },
-                title = { Text("Edit Profile Name", color = Color.White) },
+                title = { Text("Изменить имя профиля", color = Color.White) },
                 text = {
                     BasicTextField(
                         value = tempName,
@@ -170,7 +177,7 @@ fun ProfileScreen(
                 confirmButton = {
                     TextButton(
                         onClick = {
-                            if (tempName.isNotBlank()) { // Проверка на непустое значение
+                            if (tempName.isNotBlank()) {
                                 ProfileName = tempName
                             }
                             tempName = ""
@@ -187,7 +194,7 @@ fun ProfileScreen(
                             showNameDialog = false
                         }
                     ) {
-                        Text("Cancel", color = Color.White)
+                        Text("Отмена", color = Color.White)
                     }
                 },
                 containerColor = Color(0xFF2C2F3E),
@@ -195,12 +202,143 @@ fun ProfileScreen(
             )
         }
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(30.dp))
 
+        // Прокручиваемая часть с текстовыми полями
+        LazyColumn(
+            modifier = Modifier
+                .weight(1f) // Занимает оставшееся пространство
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            item {
+                Text(
+                    text = Level.toString(),
+                    color = Color.White,
+                    fontSize = 36.sp,
+                    fontWeight = FontWeight.Medium,
+                    modifier = Modifier.padding(4.dp)
+                )
+                Text(
+                    text = "Уровень",
+                    color = Color.White,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Light,
+                    modifier = Modifier.padding(4.dp)
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+            }
 
+            item {
+                Text(
+                    text = Score.toString(),
+                    color = Color.White,
+                    fontSize = 36.sp,
+                    fontWeight = FontWeight.Medium,
+                    modifier = Modifier.padding(4.dp)
+                )
+                Text(
+                    text = "Баллов получено",
+                    color = Color.White,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Light,
+                    modifier = Modifier.padding(4.dp)
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+            }
+
+            item {
+                Text(
+                    text = Forecasts.toString(),
+                    color = Color.White,
+                    fontSize = 36.sp,
+                    fontWeight = FontWeight.Medium,
+                    modifier = Modifier.padding(4.dp)
+                )
+                Text(
+                    text = "Предсказано игр",
+                    color = Color.White,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Light,
+                    modifier = Modifier.padding(4.dp)
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+            }
+
+            item {
+                Text(
+                    text = "${(ScorePoints.toFloat() / Forecasts * 100).toInt()}%",
+                    color = Color.White,
+                    fontSize = 36.sp,
+                    fontWeight = FontWeight.Medium,
+                    modifier = Modifier.padding(4.dp)
+                )
+                Text(
+                    text = "Верно предсказан счет",
+                    color = Color.White,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Light,
+                    modifier = Modifier.padding(4.dp)
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+            }
+
+            item {
+                Text(
+                    text = "${(WinnerPoints.toFloat() / Forecasts * 100).toInt()}%",
+                    color = Color.White,
+                    fontSize = 36.sp,
+                    fontWeight = FontWeight.Medium,
+                    modifier = Modifier.padding(4.dp)
+                )
+                Text(
+                    text = "Верно предсказан победитель",
+                    color = Color.White,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Light,
+                    modifier = Modifier.padding(4.dp)
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+            }
+
+            item {
+                Text(
+                    text = Followers.toString(),
+                    color = Color.White,
+                    fontSize = 36.sp,
+                    fontWeight = FontWeight.Medium,
+                    modifier = Modifier.padding(4.dp)
+                )
+                Text(
+                    text = "Подписчики",
+                    color = Color.White,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Light,
+                    modifier = Modifier.padding(4.dp)
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+            }
+
+            item {
+                Text(
+                    text = Following.toString(),
+                    color = Color.White,
+                    fontSize = 36.sp,
+                    fontWeight = FontWeight.Medium,
+                    modifier = Modifier.padding(4.dp)
+                )
+                Text(
+                    text = "Подписки",
+                    color = Color.White,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Light,
+                    modifier = Modifier.padding(4.dp)
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+            }
         }
     }
-
+}
 
 @Composable
 fun ProfileTopBar(onMenuClick: () -> Unit) {
@@ -244,4 +382,3 @@ fun ProfileCenterContent(modifier: Modifier = Modifier) {
         )
     }
 }
-
