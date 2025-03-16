ab1b37238e4ff150abfbdf8d492e9937aa18fd18
AnjeZenda <baykov200396@gmail.com>
2025-03-16 16:23:51 +0300

    Added popup to login screen

--- frontend/app/src/main/java/com/khl_app/ui/screens/auth/LoginScreen.kt
+++ frontend/app/src/main/java/com/khl_app/ui/screens/auth/LoginScreen.kt
@@ -4,9 +4,12 @@ import MainViewModel
 import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -33,14 +36,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.khl_app.ui.screens.AboutPopUp
 
 @Composable
 fun LoginScreen(
@@ -54,6 +60,7 @@ fun LoginScreen(
     var password by remember { mutableStateOf("") }
     var errorMessage by remember { mutableStateOf(defaultErrorMsg) }
     var isLoading by remember { mutableStateOf(false) }
+    var isAboutVisible by remember { mutableStateOf(false) }
 
     val context = LocalContext.current
 
@@ -233,18 +240,35 @@ fun LoginScreen(
 //                )
 //            }
 
-            Box(
+            Column (
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(top = 40.dp),
-                contentAlignment = Alignment.Center
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text(
                     text = "Мы не относимся к КХЛ и не несем ответственности за содержание",
                     fontSize = 12.sp,
                     color = lightGrayText,
-                    textAlign = TextAlign.Center
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.border(width = 1.dp, shape = RoundedCornerShape(5.dp), color = lightGrayButton)
                 )
+                Spacer(Modifier.width(10.dp))
+                Text(
+                    text = "About app",
+                    modifier = Modifier.clickable {
+                        isAboutVisible = true
+                    },
+                    color = lightGrayText,
+                    fontSize = 12.sp,
+                    textDecoration = TextDecoration.Underline,
+                )
+            }
+
+        }
+        if (isAboutVisible) {
+            AboutPopUp {
+                isAboutVisible = false
             }
         }
     }
