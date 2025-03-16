6e6f5fc8cfbcee3c710b7faddd93c24229ad26db
defrozentruth <metro2033dg@gmail.com>
2025-03-16 22:11:51 +0300

    updates

--- frontend/app/src/main/java/com/khl_app/ui/screens/client/FollowersScreen.kt
+++ frontend/app/src/main/java/com/khl_app/ui/screens/client/FollowersScreen.kt
@@ -1,18 +1,44 @@
 package com.khl_app.presentation.screens
 
 import MainViewModel
-import android.app.Activity
-import android.content.Intent
 import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,18 +46,17 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import com.khl_app.storage.getUserIdFromToken
 import com.khl_app.ui.navigation.Screen
 import com.khl_app.ui.presentation.FollowersUiState
 import com.khl_app.ui.presentation.FollowersViewModel
 import com.khl_app.ui.presentation.SubscriptionState
+import com.khl_app.ui.screens.AboutPopUp
 import com.khl_app.ui.screens.client.BottomPanel
 import com.khl_app.ui.screens.client.FollowerItem
-import kotlinx.coroutines.launch
-import com.khl_app.storage.getUserIdFromToken
-import com.khl_app.ui.screens.AboutPopUp
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -55,7 +80,6 @@ fun FollowersScreen(
     var userIdInput by remember { mutableStateOf("") }
     var ownUserId by remember { mutableStateOf("") }
 
-    // Получение ID пользователя для определения своего профиля
     LaunchedEffect(Unit) {
         mainViewModel.tokenCache.getInfo().collect { tokenData ->
             ownUserId = getUserIdFromToken(tokenData.accessToken)!!
@@ -84,7 +108,6 @@ fun FollowersScreen(
         }
     }
 
-    // Диалог для ввода ID пользователя
     if (showSubscribeDialog) {
         AlertDialog(
             onDismissRequest = {
@@ -252,7 +275,6 @@ fun FollowersScreen(
                 }
             }
 
-            // Индикатор загрузки при подписке/отписке
             if (subscriptionState is SubscriptionState.Loading) {
                 Box(
                     modifier = Modifier
