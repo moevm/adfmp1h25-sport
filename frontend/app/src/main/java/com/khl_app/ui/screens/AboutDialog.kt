3982c56bd65959c678d258aa2ce40e657ad84596
AnjeZenda <baykov200396@gmail.com>
2025-03-16 16:10:37 +0300

    Added about dialog

---
+++ frontend/app/src/main/java/com/khl_app/ui/screens/AboutDialog.kt
@@ -0,0 +1,81 @@
+package com.khl_app.ui.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.DialogProperties
+import com.khl_app.ui.themes.KhlAppTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AboutPopUp(onDismiss: () -> Unit) {
+
+    BasicAlertDialog(
+        onDismissRequest = {
+            onDismiss()
+        },
+        modifier = Modifier
+            .background(color = Color.DarkGray)
+            .height(100.dp),
+        properties = DialogProperties(
+            dismissOnBackPress = true,
+            dismissOnClickOutside = true,
+            usePlatformDefaultWidth = true
+        ),
+    ) {
+        Column(
+            modifier = Modifier.wrapContentHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top,
+        ) {
+            Text(
+                text = "О приложении",
+                color = Color.White,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier.padding(10.dp)
+            )
+            Text(
+                text = "Приложение предназначено для прогнозирования матчей и отслеживание статистики прогнозов пользователя и его друзей",
+                color = Color.White,
+                fontSize = 10.sp,
+                lineHeight = 12.sp,
+                fontWeight = FontWeight.Normal,
+                modifier = Modifier.padding(horizontal = 10.dp),
+                textAlign = TextAlign.Justify
+            )
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun AboutPreview() {
+    KhlAppTheme {
+        AboutPopUp({})
+    }
+}
--- frontend/app/src/main/java/com/khl_app/ui/screens/client/ProfileScreen.kt
+++ frontend/app/src/main/java/com/khl_app/ui/screens/client/ProfileScreen.kt
@@ -4,25 +4,47 @@ import MainViewModel
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import coil.transform.CircleCropTransformation
-import androidx.compose.ui.platform.LocalContext
-import androidx.navigation.NavHostController
 import com.khl_app.ui.navigation.Screen
+import com.khl_app.ui.screens.AboutPopUp
 import kotlinx.coroutines.launch
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
@@ -43,6 +65,7 @@ fun ProfileScreen(
     var tempName by remember { mutableStateOf("") }
     val state = rememberModalBottomSheetState()
     val scope = rememberCoroutineScope()
+    var aboutIsVisible by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier
@@ -103,7 +126,10 @@ fun ProfileScreen(
                             .fillMaxWidth()
                             .background(Color(0xFF1D1F2B), RoundedCornerShape(8.dp))
                             .padding(8.dp),
-                        textStyle = LocalTextStyle.current.copy(color = Color.White, fontSize = 16.sp)
+                        textStyle = LocalTextStyle.current.copy(
+                            color = Color.White,
+                            fontSize = 16.sp
+                        )
                     )
                 },
                 confirmButton = {
@@ -172,7 +198,10 @@ fun ProfileScreen(
                             .fillMaxWidth()
                             .background(Color(0xFF1D1F2B), RoundedCornerShape(8.dp))
                             .padding(8.dp),
-                        textStyle = LocalTextStyle.current.copy(color = Color.White, fontSize = 16.sp)
+                        textStyle = LocalTextStyle.current.copy(
+                            color = Color.White,
+                            fontSize = 16.sp
+                        )
                     )
                 },
                 confirmButton = {
@@ -223,13 +252,21 @@ fun ProfileScreen(
                         }
                     }
                 },
+                onAbout = {
+                    aboutIsVisible = true
+                },
                 onTrackable = {},
                 scope = scope,
                 state = state
             )
         }
+        if (aboutIsVisible) {
+            AboutPopUp {
+                aboutIsVisible = false
+            }
         }
     }
+}
 
 
 @Composable
